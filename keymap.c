4602e39cf1999aec8d94ef075c3700db62be0f49
emombay <emombay@gmail.com>
2016-02-03 20:20:13 -0500

    Colemak is now default! Argh

--- keymap.c
+++ keymap.c
@@ -2,25 +2,25 @@
 #include "debug.h"
 #include "action_layer.h"
 
-#define BASE 0 // default layer
-#define DVOR 1 // Dvorak layer
-#define COLE 2 // Colemak layer
-#define SYMB 3 // symbols
-#define MDIA 4 // media keys
+#define COLE 0 // Colemak layer
+#define BASE 1 // default layer
+//#define DVOR 1  Dvorak layer
+#define SYMB 2 // symbols
+#define MDIA 3 // media keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Keymap 0: Colemak layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |  L3  |           |  L3  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |  L2  |           |  L2  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Esc  |           | Ctr+ |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   F  |   P  |   G  | Esc  |           | Ctr+ |   J  |   L  |   U  |   Y  |   ;  |   \    |
  * |--------+------+------+------+------+------|      |           | Win  |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
+ * | Ctrl   |   A  |   R  |   S  |   T  |   D  |------|           |------|   H  |   N  |   E  |   I  |   O  |   '    |
  * |--------+------+------+------+------+------|  _   |           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   K  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|AltTab| LGui | Left | Right|                                       |  Up  | Down |   [  |   ]  | ->L1  |
+ *   |Grv/L1|AltTab| LGui | Left | Right|                                       |  Up  | Down |   [  | ]/L3 | ->L1 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | Del  |       | Alt  |Ctrl    |
@@ -32,26 +32,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = KEYMAP(  // layer 0 : default
+[COLE] = KEYMAP(  // layer 0 : Colemak
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(3),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
-        KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_UNDS,
-        LT(SYMB,KC_GRV),M(1),         KC_LGUI,KC_LEFT,KC_RGHT,
+        KC_EQL,         KC_1,         	KC_2,    KC_3,   KC_4,   KC_5,    	TG(SYMB),
+        KC_TAB,         KC_Q,         	KC_W,    KC_F,   KC_P,   KC_G,   	KC_ESC,
+        KC_LCTL,        KC_A,         	KC_R,    KC_S,   KC_T,   KC_D,
+        KC_LSFT,        KC_Z, 			KC_X,    KC_C,   KC_V,   KC_B,   	KC_UNDS,
+        LT(SYMB,KC_GRV),M(1),      		KC_LGUI, KC_LEFT,KC_RGHT,
                                               ALT_T(KC_APP),  KC_DELT,
                                                               KC_HOME,
                                                 KC_SPC,KC_BSPC,KC_END,
         // right hand
-             TG(3),       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-      LCTL(LGUI(KC_NO)),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN3,
-             KC_LALT,        KC_RCTL,
-             KC_PGUP,
-             KC_PGDN,KC_ENT, KC_SPC
+            TG(SYMB),     		KC_6,   KC_7,   KC_8,   KC_9,   KC_0,            KC_MINS,
+            LCTL(LGUI(KC_NO)),  KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,         KC_BSLS,
+								KC_H,   KC_N,   KC_E,   KC_I,   KC_O, 			 KC_QUOT,
+            MEH_T(KC_NO),		KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,      	 KC_RSFT,
+										KC_UP,  KC_DOWN,KC_LBRC,LT(MDIA,KC_RBRC),KC_FN2,
+			KC_LALT,        KC_RCTL,
+			KC_PGUP,
+			KC_PGDN,KC_ENT, KC_SPC
     ),
+
 
 /* Keymap 1: Dvorak layer
  *
@@ -73,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |------|       |------|        |      |
  *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
- */
+
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [DVOR] = KEYMAP(  // layer 1 : Dvorak
@@ -95,51 +96,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TRNS,        KC_TRNS,
              KC_TRNS,
              KC_TRNS,KC_TRNS, KC_TRNS
-    ),
-/* Keymap 2: Colemak layer
+    ),*/
+/* Keymap 1: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |  L2  |           |  L2  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   Q  |   W  |   F  |   P  |   G  |      |           |      |   J  |   L  |   U  |   Y  |   ;  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   A  |   R  |   S  |   T  |   D  |------|           |------|   H  |   N  |   E  |   I  |O / L2|   '    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   Z  |   X  |   C  |   V  |   B  |      |           |      |   K  |   M  |   ,  |   .  |   /  |        |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Esc  |           | Ctr+ |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           | Win  |------+------+------+------+------+--------|
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|  _   |           | Meh  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |  ->L0  |
+ *   |Grv/L2|AltTab| LGui | Left | Right|                                       |  Up  | Down |   [  | ]/L3 | ->L0 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |        |
+ *                                        | App  | Del  |       | Alt  |Ctrl    |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |      |        |      |
- *                                 |      |      |------|       |------|        |      |
- *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 | Space|Back  |------|       |------| Enter  |Space |
+ *                                 |      |Space | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
-[COLE] = KEYMAP(  // layer 2 : Colemak
+ [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_TRNS,        KC_TRNS,        KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,   KC_TRNS,
-        KC_TRNS,        KC_Q,         	KC_W,    KC_F,   KC_P,   KC_G,   	KC_TRNS,
-        KC_TRNS,        KC_A,         	KC_R,    KC_S,   KC_T,   KC_D,
-        KC_TRNS,        KC_Z, 			KC_X,    KC_C,   KC_V,   KC_B,   	KC_TRNS,
-        KC_TRNS,		KC_TRNS,      	KC_TRNS, KC_TRNS,KC_TRNS,
-													 KC_TRNS,  KC_TRNS,
-                                                               KC_TRNS,
-                                               KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(SYMB),
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_ESC,
+        KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_UNDS,
+        LT(SYMB,KC_GRV),M(1),         KC_LGUI,KC_LEFT,KC_RGHT,
+                                              ALT_T(KC_APP),  KC_DELT,
+                                                              KC_HOME,
+                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
-             KC_TRNS,     	KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_BSLS,
-             KC_TRNS,       KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,        KC_SLSH,
-							KC_H,   KC_N,   KC_E,   KC_I,   LT(MDIA, KC_O), KC_MINS,
-             KC_TRNS,		KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,      	KC_TRNS,
-									KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_FN2,
-             KC_TRNS,        KC_TRNS,
-             KC_TRNS,
-             KC_TRNS,KC_TRNS, KC_TRNS
+             TG(SYMB),    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+      LCTL(LGUI(KC_NO)),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
+             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                                  KC_UP,  KC_DOWN,KC_LBRC,LT(MDIA, KC_RBRC),KC_FN1,
+             KC_LALT,        KC_RCTL,
+             KC_PGUP,
+             KC_PGDN,KC_ENT, KC_SPC
     ),
-/* Keymap 3: Symbol Layer
+
+/* Keymap 2: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | PrintSc|  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
@@ -181,7 +181,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
-/* Keymap 4: Media and mouse keys
+/* Keymap 3: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
@@ -225,10 +225,10 @@ KEYMAP(
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),                // FN1 - Momentary Layer 1 (Symbols)
-	[2] = ACTION_LAYER_SET_CLEAR(COLE),					// FN2 - Go to Base Layer
-	[3] = ACTION_LAYER_SET(DVOR, ON_RELEASE),			// FN3 - Go to Dvorak Layer
-	[4] = ACTION_LAYER_SET(COLE, ON_RELEASE) 			// FN4 - Go to Colemak Layer
+    [0] = ACTION_LAYER_TAP_TOGGLE(SYMB),                // FN1 - Momentary Layer 1 (Symbols)
+	[1] = ACTION_LAYER_SET_CLEAR(BASE),					// FN2 - Go to Base Layer
+	//[3] = ACTION_LAYER_SET(DVOR, ON_RELEASE),			 FN3 - Go to Dvorak Layer
+	[2] = ACTION_LAYER_SET(BASE, ON_RELEASE) 			// FN3 - Go to default Colemak Layer
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -274,12 +274,8 @@ void * matrix_scan_user(void) {
             break;
         case 2:			
 			ergodox_right_led_1_on();
-			ergodox_right_led_3_on();
             break;
 		case 3:			
-            ergodox_right_led_1_on();
-			break;
-		case 4:			
             ergodox_right_led_2_on();
 			break;
         default:
