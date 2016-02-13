2d14a7e3d1abf61607bd0b3a88d7444a7bf69c64
emombay <emombay@gmail.com>
2016-02-13 09:57:17 -0500

    switched alt around

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |Grv/L1|AltTab| LGui | Left | Right|                                       |  Up  | Down |   [  | ]/L3 | ->L1 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | Del  |       | Alt  |Ctrl    |
+ *                                        | Alt  | Del  |       |App/Alt|Ctrl   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space|Back  |------|       |------| Enter  |Space |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_A,         	KC_R,    KC_S,   KC_T,   KC_D,
         KC_LSFT,        KC_Z, 			KC_X,    KC_C,   KC_V,   KC_B,   	KC_UNDS,
         LT(SYMB,KC_GRV),M(1),      		KC_LGUI, KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_DELT,
+                                                KC_LALT,  KC_DELT,
                                                               KC_HOME,
                                                 KC_SPC,KC_BSPC,KC_END,
         // right hand
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 								KC_H,   KC_N,   KC_E,   KC_I,   KC_O, 			 KC_QUOT,
             MEH_T(KC_NO),		KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,      	 KC_RSFT,
 										KC_UP,  KC_DOWN,KC_LBRC,LT(MDIA,KC_RBRC),KC_FN2,
-			KC_LALT,        KC_RCTL,
+			ALT_T(KC_APP),        KC_RCTL,
 			KC_PGUP,
 			KC_PGDN,KC_ENT, KC_SPC
     ),
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_UNDS,
         LT(SYMB,KC_GRV),M(1),         KC_LGUI,KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_DELT,
+                                                KC_LALT,  KC_DELT,
                                                               KC_HOME,
                                                 KC_SPC,KC_BSPC,KC_END,
         // right hand
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                   KC_UP,  KC_DOWN,KC_LBRC,LT(MDIA, KC_RBRC),KC_FN1,
-             KC_LALT,        KC_RCTL,
+             ALT_T(KC_APP),        KC_RCTL,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
     ),
