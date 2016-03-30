2d5be23c4d7595769de411e9ed1322f87ee8df8e
Ernest <emombay@lsvusa.com>
2016-03-30 09:44:51 -0400

    fixing broken chain actions with macros

--- keymap.c
+++ keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,         KC_1,         	KC_2,    KC_3,   KC_4,   KC_5,    	LCTL(KC_S),
         KC_TAB,         KC_Q,         	KC_W,    KC_F,   KC_P,   KC_G,   	KC_DELT,
         KC_FN0,        	KC_A,         	KC_R,    KC_S,   KC_T,   KC_D,
-        KC_LSFT,        KC_Z, 			KC_X,    KC_C,   KC_V,   KC_B,   	KC_UNDS,
-      RCTL(RSFT(KC_NO)),M(1),      		KC_LALT, LCTL(KC_Z),LCTL(KC_Y),
+        KC_LSFT,        KC_Z, 					KC_X,    KC_C,   KC_V,   KC_B,   	KC_UNDS,
+				M(3),						M(1),      		KC_LALT, LCTL(KC_Z),LCTL(KC_Y),
                                                 KC_LGUI,  KC_ESC,
                                                               KC_HOME,
                                             CTL_T(KC_SPC),KC_BSPC,KC_END,
         // right hand
             TG(SYMB),     		KC_6,   KC_7,   KC_8,   KC_9,   KC_0,            KC_MINS,
-            LCTL(LALT(KC_NO)),  KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,         KC_BSLS,
+            M(2),  KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,         KC_BSLS,
 								KC_H,   KC_N,   KC_E,   KC_I,   KC_O, 			 KC_QUOT,
             MEH_T(KC_NO),		KC_K,   KC_M,   KC_COMM,KC_DOT, LT(MDIA,KC_SLSH),KC_RSFT,
 										KC_LEFT,KC_UP,  KC_DOWN,KC_RGHT,KC_FN2,
@@ -247,6 +247,18 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 			return MACRO( D(LALT), D(TAB), W(200), U(TAB), END );
 		} else {
 			return MACRO( U(LALT), END );
+		}
+		case 2: 
+		if (record->event.pressed) {
+			return MACRO( D(LCTL), D(LGUI), END );
+		} else {
+			return MACRO( U(LGUI), U(LCTL), END );
+		}
+		case 3: 
+		if (record->event.pressed) {
+			return MACRO( D(LCTL), D(LSFT), END );
+		} else {
+			return MACRO( U(LSFT), U(LCTL), END );
 		}
 		break;
       }
